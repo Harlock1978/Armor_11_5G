a88c97e1402a2138f1128859b377b0a9141c0f14
Harlock1978 <c.tappero@gmail.com>
2022-11-21 19:28:05 +0100

    Start

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Ulefone Armor 11 5G (Armor_11_5G)
+
+```
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_Armor_11_5G.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Armor_11_5G device
+$(call inherit-product, device/ulefone/Armor_11_5G/device.mk)
+
+PRODUCT_DEVICE := Armor_11_5G
+PRODUCT_NAME := omni_Armor_11_5G
+PRODUCT_BRAND := Ulefone
+PRODUCT_MODEL := Armor 11 5G
+PRODUCT_MANUFACTURER := ulefone
+
+PRODUCT_GMS_CLIENTID_BASE := android-gotron
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_k6873v1_64-user 10 QP1A.190711.020 mp2k6885v164P10 release-keys"
+
+BUILD_FINGERPRINT := Ulefone/Armor_11_5G_EEA/Armor_11_5G:10/QP1A.190711.020/1608088743:user/release-keys
