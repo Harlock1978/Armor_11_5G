34ab328def9a244d2a643f109236e0d5354cf5a9
Harlock1978 <c.tappero@gmail.com>
2022-11-23 00:05:39 +0100

    Second update

--- omni_Armor_11_5G.mk
+++ omni_Armor_11_5G.mk
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -24,6 +26,14 @@ PRODUCT_MANUFACTURER := ulefone
 PRODUCT_GMS_CLIENTID_BASE := android-gotron
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=Armor_11_5G \
+    PRODUCT_NAME=Armor_11_5G_EEA \
     PRIVATE_BUILD_DESC="full_k6873v1_64-user 10 QP1A.190711.020 mp2k6885v164P10 release-keys"
 
 BUILD_FINGERPRINT := Ulefone/Armor_11_5G_EEA/Armor_11_5G:10/QP1A.190711.020/1608088743:user/release-keys
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1
