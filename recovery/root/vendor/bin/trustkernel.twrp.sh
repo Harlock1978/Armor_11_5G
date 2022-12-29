140f3d435cb680bf7128d36d65d228d51a43c576
Harlock1978 <c.tappero@gmail.com>
2022-12-29 20:23:17 +0100

    Update Trustkernel

--- recovery/root/vendor/bin/trustkernel.twrp.sh
+++ recovery/root/vendor/bin/trustkernel.twrp.sh
@@ -3,3 +3,7 @@
 mkdir -p /mnt/vendor/persist/t6_twrp
 rm -rf /mnt/vendor/persist/t6_twrp/*
 cp -rf /mnt/vendor/persist/t6/* /mnt/vendor/persist/t6_twrp
+
+mkdir -p /mnt/vendor/protect_f/tee_twrp
+rm -rf /mnt/vendor/protect_f/tee_twrp/*
+cp -rf //mnt/vendor/protect_f/tee/* //mnt/vendor/protect_f/tee_twrp
